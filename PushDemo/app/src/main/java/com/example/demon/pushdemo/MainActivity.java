0f462e185fc442241a0570388afa84ad1b8cbb90
yangjunqiang <yangjunqiang90@163.com>
2017-04-03 01:03:57 +0800

    show page but not execute function demo

---
+++ PushDemo/app/src/main/java/com/example/demon/pushdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.demon.pushdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.baidu.butler.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
